0bc2a22c6947a83da5fb54aee16aef77e59ac7a2
THAIS RODRIGUES ANDRADE <thais.randrade5@senacsp.edu.br>
2023-10-27 20:20:51 -0300

    tudo ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {MyDialogComponent} from './my-dialog/my-dialog.component';
-
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Task } from 'src/Task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoo';
-  constructor(public dialog: MatDialog){}
-  //metodo pra abrir nosso dialog
-  openDialog(): void{
-    this.dialog.open(MyDialogComponent)
+  title = 'todo-list';
+  constructor(public dialog: MatDialog) { }
+  tarefas: Task[] = [
+    new Task({
+      name: "Estudar Angular",
+      descricao: "Preciso estudar angular..."
+    })
+  ]
+  openDialog(): void {
+    const dialogRef = this.dialog.open(MyDialogComponent);
+    dialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        this.tarefas.push(res);
+      }
+    })
+  }
+  get totalDeTarefas(): number {
+    return this.tarefas.length
+  }
+
+  get totalDePendentes(): number{
+    return this.tarefas.filter(tarefa=>tarefa.situacao === "Pendente").length
+
+  }
+  alterarSituacao(tarefa: Task, situacao: string): void{
+    tarefa.situacao = situacao
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatExpansionModule} from  '@angular/material/expansion';
-import { MyDialogComponent } from './my-dialog/my-dialog.component' ;
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+
 
 @NgModule({
   declarations: [
@@ -21,7 +26,11 @@ import { MatIconModule} from '@angular/material/icon';
     MatChipsModule,
     MatExpansionModule,
     MatDialogModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
